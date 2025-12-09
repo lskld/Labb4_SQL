f1cab2721bef132d21c02b7a42c080200a689e71
lskld <theo.lillieskold@gmail.com>
2025-12-09 23:35:52 +0100

    Added DbContext and possibility to connect to SQL Database through connection string

---
+++ Models/SchoolDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Labb4_SQL.Models
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext() { }
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+            if (string.IsNullOrEmpty(connectionString))
+                throw new InvalidOperationException("Connection string is missing");
+            
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
