45844b339919c39d00b1a57a5e321b5421375eb1
lskld <theo.lillieskold@gmail.com>
2025-12-11 14:47:16 +0100

    Add a main menu using Spectre Console

---
+++ Menus.cs
@@ -0,0 +1,25 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labb4_SQL
+{
+    public class Menus
+    {
+        public static void MainMenu()
+        {
+            var menuChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Welcome to the Schools internal Databse. Please use the menu below to proceed.")
+                .PageSize(10)
+                .AddChoices(new[]
+                {
+                    "Check amount of teachers in each department",
+                    "Show information about all students",
+                    "See list of active courses",
+                    "Grade a student"
+                }));            
+        }
+    }
+}
