2621c42a87f8b9296e055dcf9b3f6867a8eab98c
lskld <theo.lillieskold@gmail.com>
2025-12-14 23:21:32 +0100

    Update the SQL Querys

--- sql_files/SchoolDb_Labb4_Assignment_Querys.sql
+++ sql_files/SchoolDb_Labb4_Assignment_Querys.sql
@@ -1,4 +1,23 @@
-USE SchoolDb
+-- Creates stored procedure that shows all necessary student information from a student Id
+
+CREATE PROCEDURE ShowStudentInformation
+(@SID INT)
+AS
+BEGIN
+	SELECT 
+		s.FirstName AS 'First Name', 
+		s.LastName AS 'Last Name', 
+		s.PersonalNumber AS 'Personal Number', 
+		c.ClassName AS 'Class Name'
+		FROM Students s
+	INNER JOIN Classes c ON c.ClassId = s.ClassId
+	WHERE S.StudentId = @SID
+END
+GO
+
+-- Use the stored procedure that was made above
+
+EXEC ShowStudentInformation 51 -- <== Input any Student Id here 
 
 -- Shows all departments and their average monthly salary
 
@@ -11,3 +30,8 @@ GROUP BY DepartmentName
 SELECT DepartmentName AS 'Department Name', SUM(Salary) AS 'Total Salary / month' FROM Employees 
 INNER JOIN Departments ON Departments.DepartmentId = Employees.DepartmentId
 GROUP BY DepartmentName 
+
+-- Create stored procedure that taked a studentId and returns important information about that student
+
+
+
