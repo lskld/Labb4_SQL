3e576a4ba1df8d4c3facc3fc0930cca4c51e21e0
lskld <theo.lillieskold@gmail.com>
2025-12-15 16:55:18 +0100

    Finalize assignment querys

--- sql_files/SchoolDb_Labb4_Assignment_Querys.sql
+++ sql_files/SchoolDb_Labb4_Assignment_Querys.sql
@@ -1,4 +1,9 @@
--- Creates stored procedure that shows all necessary student information from a student Id
+-- This document contains all querys that show the database meet the requiredments set in Labb 4.
+-- It also produces all the SQL Querys that are asked to be shown by the assigment. 
+-- //Theo Lilliesköld, NET25
+
+
+-- Create stored procedure that taked a studentId and returns important information about that student
 
 CREATE PROCEDURE ShowStudentInformation
 (@SID INT)
@@ -31,7 +36,57 @@ SELECT DepartmentName AS 'Department Name', SUM(Salary) AS 'Total Salary / month
 INNER JOIN Departments ON Departments.DepartmentId = Employees.DepartmentId
 GROUP BY DepartmentName 
 
--- Create stored procedure that taked a studentId and returns important information about that student
+-- Show overview of all school employees, their roles, their work time and salary
 
+SELECT e.FirstName, e.LastName, r.RoleName, e.YearsEmployed, e.Salary FROM Employees e
+INNER JOIN Roles r ON r.RoleId = e.RoleId
 
+-- Create a stored procedure to create new employees
 
+CREATE PROCEDURE CreateNewEmployee
+	@FirstName NVARCHAR(100),
+	@LastName NVARCHAR(100),
+	@YearsEmployed INT,
+	@Salary INT,
+	@DepartmentId INT,
+	@RoleId INT
+AS
+BEGIN
+	INSERT INTO Employees (FirstName, LastName, YearsEmployed, Salary, DepartmentId, RoleId)
+	VALUES(@FirstName, @LastName, @YearsEmployed, @Salary, @DepartmentId, @RoleId)
+END
+GO
+
+-- Using the stored procedure created above
+
+EXEC CreateNewEmployee Alexander, Fennell, 1, 35000, 1, 2
+
+-- Create a stored procedure to create new students. Students also contain which class they are in
+
+CREATE PROCEDURE CreateNewStudent
+	@FirstName NVARCHAR(100),
+	@LastName NVARCHAR(100),
+	@PersonalNumber NVARCHAR(13),
+	@ClassId INT
+AS
+BEGIN
+	INSERT INTO Students (FirstName, LastName, PersonalNumber, ClassId)
+	VALUES(@FirstName, @LastName, @PersonalNumber, @ClassId)
+END 
+GO
+
+-- Create a stored procedure to create new grades. 
+-- Grades contain the student, grading teacher, date and course
+
+CREATE PROCEDURE SetNewGrade
+	@SetGrade NVARCHAR(1),
+	@GradingDate DATE,
+	@EmployeeId INT,
+	@StudentId INT,
+	@CourseId INT
+AS
+BEGIN
+	INSERT INTO Grades (SetGrade, GradingDate, EmployeeId, StudentId, CourseId)
+	VALUES(@SetGrade, @GradingDate, @EmployeeId, @StudentId, @CourseId)
+END
+GO
